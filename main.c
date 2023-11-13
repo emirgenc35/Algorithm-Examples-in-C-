16e2f2c7b6ada8be6f6c6bb37f1a5df1ff0dfc9a
emirgenc35 <148959269+emirgenc35@users.noreply.github.com>
2023-11-13 09:42:18 +0300

    ALGORITHM THAT GIVES THE SUM OF TWO NUMBERS

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Ýki sayýyý Toplamak için gerekli algoritma
+
+    int sayi_1 ,sayi_2,toplam = 0;
+
+    printf("Birinci Sayiyi Giriniz :");
+    scanf("%d",&sayi_1);
+
+    printf("Ikinci Sayiyi Giriniz :");
+    scanf("%d",&sayi_2);
+
+    toplam = sayi_1 + sayi_2 ;
+
+    printf("Iki Sayinin Toplami : %d",toplam);
+
+
+
+
+    return 0;
+}
